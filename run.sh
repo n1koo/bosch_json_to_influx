7ecf6520e582a4a0efb494e3fe0d74a805422004
Niko Kurtti <niko@kurtti.eu>
2021-09-03 15:54:23 +0300

    Add hot water scraping

--- run.sh
+++ run.sh
@@ -9,7 +9,9 @@ for name in BOSCH_IP BOSCH_TOKEN BOSCH_PASSWORD INFLUX_DB_HOST INFLUX_DB_DATABAS
     fi
 done
 
-echo "Scanning pump"
-bosch_cli scan -s SENSORS --host $BOSCH_IP --protocol HTTP --device IVT --token $BOSCH_TOKEN --password $BOSCH_PASSWORD -o sensor_scan.json
-echo "Dumping data to influx"
-python ./bosch_json_to_influx.py --influx_db_host $INFLUX_DB_HOST --influx_db_database $INFLUX_DB_DATABASE --influx_db_user $INFLUX_DB_USER --influx_db_password $INFLUX_DB_PASSWORD --pump_name $PUMP_NAME
+echo "Sensors"
+bosch_cli scan -s SENSORS --host $BOSCH_IP --protocol HTTP --device IVT --token $BOSCH_TOKEN --password $BOSCH_PASSWORD -o sensors.json
+python ./bosch_json_to_influx.py --influx_db_host $INFLUX_DB_HOST --influx_db_database $INFLUX_DB_DATABASE --influx_db_user $INFLUX_DB_USER --influx_db_password $INFLUX_DB_PASSWORD --pump_name $PUMP_NAME --input_json_file sensors.json
+echo "DHW"
+bosch_cli scan -s dhw --host $BOSCH_IP --protocol HTTP --device IVT --token $BOSCH_TOKEN --password $BOSCH_PASSWORD -o dhw.json
+python ./bosch_json_to_influx.py --influx_db_host $INFLUX_DB_HOST --influx_db_database $INFLUX_DB_DATABASE --influx_db_user $INFLUX_DB_USER --influx_db_password $INFLUX_DB_PASSWORD --pump_name $PUMP_NAME --input_json_file dhw.json
